de469e3f9c767874113b1988f8f7ee5588288502
Sigmund <isaias.salgado21@gmail.com>
2023-11-25 22:10:51 -0300

    mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://da5f879d-43e4-4606-b4dc-b0e989c97145.serverhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
